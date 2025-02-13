7f8b5c0ff39862fae585e05a26ef4f7459579a57
Keith Solomon <keith@vincentdesign.ca>
2025-02-13 14:25:09 -0600

    🐞 fix: Update updater settings

--- resource-filter.php
+++ resource-filter.php
@@ -16,13 +16,13 @@ function cfInit() {
     $config = array(
       'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
       'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-      'api_url' => 'https://api.github.com/repos/username/repository-name', // the GitHub API url of your GitHub repo
-      'raw_url' => 'https://raw.github.com/username/repository-name/master', // the GitHub raw url of your GitHub repo
-      'github_url' => 'https://github.com/username/repository-name', // the GitHub url of your GitHub repo
-      'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the GitHub repo
+      'api_url' => 'https://github.com/Vincent-Design-Inc/resource-filter', // the GitHub API url of your GitHub repo
+      'raw_url' => 'https://raw.github.com/Vincent-Design-Inc/resource-filter/master', // the GitHub raw url of your GitHub repo
+      'github_url' => 'https://github.com/Vincent-Design-Inc/resource-filter', // the GitHub url of your GitHub repo
+      'zip_url' => 'https://github.com/Vincent-Design-Inc/resource-filter/zipball/master', // the zip url of the GitHub repo
       'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-      'requires' => '3.0', // which version of WordPress does your plugin require?
-      'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+      'requires' => '6.0', // which version of WordPress does your plugin require?
+      'tested' => '6.7.2', // which version of WordPress is your plugin tested up to?
       'readme' => 'README.md', // which file to use as the readme for the version number
       'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
     );
