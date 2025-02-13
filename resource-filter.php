7497b045c470e58a0174c11b570da04cae22aea9
Keith Solomon <keith@vincentdesign.ca>
2025-02-13 14:20:24 -0600

    ✨feature: Add update from repo

--- resource-filter.php
+++ resource-filter.php
@@ -2,13 +2,36 @@
 /**
  * Plugin Name: Content Filter
  * Description: Adds filtering for the content typed by various taxonomies.
- * Version: 1.4.0
+ * Version: 1.5.0
  * Author: Keith Solomon
  */
 
 if (!defined('ABSPATH')) { exit; } // Prevent direct access
 
+require_once plugin_dir_path(__FILE__) . 'includes/updater.php';
 require_once plugin_dir_path(__FILE__) . 'includes/template-loader.php';
+
+function cfInit() {
+  if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+      'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+      'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
+      'api_url' => 'https://api.github.com/repos/username/repository-name', // the GitHub API url of your GitHub repo
+      'raw_url' => 'https://raw.github.com/username/repository-name/master', // the GitHub raw url of your GitHub repo
+      'github_url' => 'https://github.com/username/repository-name', // the GitHub url of your GitHub repo
+      'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the GitHub repo
+      'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+      'requires' => '3.0', // which version of WordPress does your plugin require?
+      'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+      'readme' => 'README.md', // which file to use as the readme for the version number
+      'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
+    );
+
+    new WpGitHubUpdater($config);
+  }
+}
+
+add_action('init', 'cfInit');
 
 class ContentFilterPlugin {
   /** Registers the necessary actions and filters for the Content Filter plugin.
